d3bea1e8aed0d22982cc86173cae3ded557a51bb
Josh Reisinger <jreisi3@wgu.edu>
2025-09-19 15:35:24 -0700

    C1

--- weatherdata.py
+++ weatherdata.py
@@ -1,1 +1,19 @@
-print("test")
+class WeatherData:
+    def __init__(self), latitude, longitude, month, day, year):
+        #Provided by User
+        self.latitude = latitude
+        self.longitude = longitude
+        self.month = month
+        self.year = year
+
+        #Provided by Weather API
+        self.avg_temp = None
+        self.min_temp = None
+        self.max_temp = None
+        self.avg_wind = None
+        self.min_wind = None
+        self.max_wind = None
+        self.avg_precip = None
+        self.min_precip = None
+        self.max_precip = None
+
