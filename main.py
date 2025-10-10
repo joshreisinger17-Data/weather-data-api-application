f937724ce13500b03080cf8f98e6a34d118fcc22
Josh Reisinger <jreisi3@wgu.edu>
2025-10-10 14:12:44 -0700

    C3 create an instance of C1 and call methods of C2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ avg_temp = avg_temp_years(weather_data)
 max_wind = max_wind_years(weather_data)
 sum_precip = sum_precip_years(weather_data)
 
-#prints results
+#prints results from calls
 print(avg_temp)
 print(max_wind)
 print(sum_precip)
