d15eb49f33cf2a7f36e4153d05c29a89a93cc01c
Josh Reisinger <jreisi3@wgu.edu>
2025-10-10 13:28:50 -0700

    C3 create an instance of C1 and call methods of C2

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from C1 import WeatherData
 from C2 import avg_temp_years, max_wind_years, sum_precip_years
 
-#Instance of class
+#Instance of class created in C1
 weather_data = WeatherData(42.8865, -78.8784, 12, 25, 2023)
 
-#Calling Methods
+#Calling Methods from C2
 avg_temp = avg_temp_years(weather_data)
 max_wind = max_wind_years(weather_data)
 sum_precip = sum_precip_years(weather_data)
 
+#prints results
 print(avg_temp)
 print(max_wind)
 print(sum_precip)
