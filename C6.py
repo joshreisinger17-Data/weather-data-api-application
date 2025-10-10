55f9ce0ad495d2e92059c3a5efa44779c42c3e71
Josh Reisinger <jreisi3@wgu.edu>
2025-10-10 13:28:23 -0700

    C6 write a method to query table/get data from C5

--- C6.py
+++ C6.py
@@ -1,17 +1,18 @@
 import sqlite3
 
+#Connects to the DB and queries data from weather table from C4
 def weather_data_table():
     conn = sqlite3.connect('weather.db')
     c = conn.cursor()
-
+#executes query and fetches rows to store them
     c.execute('''Select * from weather_record''')
     rows = c.fetchall()
 
     for row in rows:
         print(row)
-
+#Closes connection to DB
     conn.close()
-
+#Call the function
 weather_data_table()
 
 
